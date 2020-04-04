61d78de808773c7dd10cf294657a7963e151df71
sebasthianfbds <sebasthian_santos@hotmail.com>
2020-04-04 19:04:26 -0300

    MongoDB

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   db: "test",
   mongodb:
-  "mongodb+srv://sebasthian:VsqrVkIFwiWA26FN@cluster0-lyonh.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://sebasthian:IFtFi121TYdwGJJ0@cluster0-lyonh.gcp.mongodb.net/test?retryWrites=true&w=majority"
 };
